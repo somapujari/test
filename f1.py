e6f09f151c2a78515985b69cb91aa23f894d8b7f
somapujari <somapujari0@gmail.com>
2026-04-08 04:54:58 +0530

    Revert "fb login page added"
    This reverts commit 18738fae8f284d83674f07ecf587270e2b48f861.

--- f1.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver 
-driver = webdriver.Chrome()
-driver.get('https://www.facebook.com') 
-driver.find_element(By.ID, 'username' ) .send_keys('soma' ) 
-driver.find_element(By.ID, 'pass' ).send_keys('1234') 
-driver.find_element(By.ID, 'button') .click()
-assert driver.title == 'Facebook sign up  or Login' , 'login not happen '
-print('end the program ' ) 
